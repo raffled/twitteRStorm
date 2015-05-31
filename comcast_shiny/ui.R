b96ed109d13c90ace264d4bdddaea161a5dfe171
raffled <raffled@gmail.com>
2015-05-31 17:35:08 -0400

    got shiny app running

--- comcast_shiny/ui.R
+++ comcast_shiny/ui.R
@@ -9,20 +9,32 @@ shinyUI(fluidPage(
             h4("Simulation"),
             sliderInput("timeInStream", "Time in Stream (min)",
                         min = 0, max = 60, value = 0, step = 1,
-                        animate = TRUE),
+                        animate = animationOptions(interval = 3000)),
             hr(),
             h4("Wordcloud Options"),
             sliderInput("wordcloud.freq", "Minimum Frequency:",
-                        min = 1,  max = 50, value = 10),
+                        min = 1,  max = 50, value = 5),
             sliderInput("wordcloud.max", "Maximum Number of Words:",
                         min = 1,  max = 300,  value = 100),
             hr(),
             h4("Polarity Cloud Options"),
             sliderInput("polarcloud.max", "Maximum Number of Words:",
-                        min = 1,  max = 300,  value = 100)
+                        min = 1,  max = 300,  value = 20)
         ), ## close sidebarPanel
         mainPanel(
-            plotOutput("wordcloud")
+            ## row for clouds
+            fluidRow(
+                column(5,
+                       h3("Word Cloud"),
+                       plotOutput("wordcloud")),
+                column(5,
+                       h3("Polarity Cloud"),
+                       plotOutput("polaritycloud"))
+            ), ## close cloud row
+            ## row for timeplots
+            fluidRow(
+                column(10, plotOutput("timeplots"))
+            ) ## close timeplot row
         ) ## close mainPanel
     ) ## close sidebarLayout
 )) ## close fluidPage/shinyUI
