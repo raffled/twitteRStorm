becb2b272eadbab19df0f842e61a78fbd19360d8
raffled <raffled@gmail.com>
2015-06-02 19:35:05 -0400

    finished new version of topology

--- twitteRStorm_v2.R
+++ twitteRStorm_v2.R
@@ -123,17 +123,42 @@ get.polarity <- function(tuple, ...){
 topo <- AddBolt(topo, Bolt(get.polarity, listen = 4, boltID = 6))
 
 track.polarity <- function(tuple, ...){
-    polarity <- tuple$polarity
-    
-    prop.df <- GetHash("prop.df")
-    if(!is.data.frame(prop.df)) prop.df <- data.frame()
-    
-    
+    polarity.df <- GetHash("polarity.df")
+    if(!is.data.frame(prop.df)) polarity.df <- data.frame()
+    polarity.df <- rbind(polarity.df,
+                         data.frame(polarity = tuple$polarity))
+    SetHash("polarity.df", polarity.df)
+    polarity <- polarity.df$polarity
+
+    polar.mat <- cbind(p.positive = (polarity == "positive"),
+                       p.neutral = (polarity == "neutral"),
+                       p.negative = (polarity == "negative"))
+    prop.df <- data.frame(t(colMeans(polar.mat, na.rm = TRUE)),
+                          t.stamp = tuple$t.stamp)
+    TrackRow("prop.df", prop.df)
 }
+topo <- AddBolt(topo, Bolt(track.polarity, listen = 6, boltID = 7))
+
+store.words.polarity <- function(tuple, ...){
+    polar.words.df <- GetHash("polar.words.df")
+    if(!is.data.frame(polar.words.df)) polar.words.df <- data.frame()
+    
+    words <- unlist(strsplit(tuple$text, " "))
+    polarity <- tuple$polarity
+
+    if(polarity %in% rownames(polar.words.df)){
+        polar.words.df[polarity,][[1]] <- list(append(polar.words.df[polarity,][[1]], words))
+    } else{
+        word.list <- list(words)
+        names(word.list) <- eval(polarity)
+        polar.words.df <- rbind(polar.words.df, as.matrix(word.list))
+    }
+    SetHash("polar.words.df", polar.words.df)
+}
+topo <- AddBolt(topo, Bolt(store.words.polarity, listen = 6, boltID = 8))
 
 #### get results
 system.time(result <- RStorm(topo))
-comcast.results <- GetHash("tweet.df", result)
 
 #### word cloud
 word.df <- GetHash("words.df", result)
@@ -141,37 +166,31 @@ words <- word.df$word
 counts <- word.df$count
 wordcloud(words, counts)
     
-wordcloud(word.vec, min.freq = 10,
+wordcloud(word.vec,
           colors = brewer.pal(8, "Dark2"),
           scale = c(4, 1))
 
 #### comparison cloud
-l.polar <- levels(foo$polarity)
-n.polar <- length(l.polar)
-by.polar <- sapply(l.polar, function(p)
-    paste(foo$text[foo$polarity == p], collapse = " "))
+polar.words.df <- GetHash("polar.words.df", result)
+by.polar <- list(positive = polar.words.df["positive",][[1]],
+                 neutral = polar.words.df["neutral",][[1]],
+                 negative = polar.words.df["negative",][[1]])
 polar.corpus <- Corpus(VectorSource(by.polar))
 polar.doc.mat <- as.matrix(TermDocumentMatrix(polar.corpus))
-colnames(polar.doc.mat) <- l.polar
-comparison.cloud(polar.doc.mat, scale = c(3, 1),
-                 max.words = 100, title.size = 2)
+colnames(polar.doc.mat) <- rownames(polar.words.df)
+comparison.cloud(polar.doc.mat)
 
 #### timeplot of polarity
 prop.df <- GetTrack("prop.df", result)
 prop.df.long <- prop.df %>% gather(Polarity, Proportion, -t.stamp)
-g1 <- ggplot(prop.df.long, aes(x = t.stamp, y = Proportion, color = Polarity)) +
+ggplot(prop.df.long, aes(x = t.stamp, y = Proportion, color = Polarity)) +
     geom_point() + geom_line() + theme(legend.position = "top")
 
 ## timeplot of tweets per minute
 tpm.df <- GetTrack("tpm.df", result)
-g2 <- ggplot(tpm.df, aes(x = t.stamp, y = tpm)) + geom_point() +
+ggplot(tpm.df, aes(x = t.stamp, y = tpm)) + geom_point() +
     geom_line()
-source("multiplot.R")
-multiplot(g1, g2)
 
-comcast_results <- list(tweets.df = comcast.results,
-                        proportion.df = prop.df,
-                        tpm.df = tpm.df)
-save(comcast_results, file = "comcast_results.bin")
 
+## set stringsAsFactors back for safety.
 options(stringsAsFactors = TRUE)
